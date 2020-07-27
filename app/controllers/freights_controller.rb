291c2c9423bf83a7587ed625d53c8ca48e47bb1c
gabriel <flaugabriel@gmail.com>
2020-07-26 22:22:52 -0400

    charge database.yml for deploy

--- app/controllers/freights_controller.rb
+++ app/controllers/freights_controller.rb
@@ -1,4 +1,6 @@
 class FreightsController < ApplicationController
+
+  # send cep and id for calculate to services
   def find_cep_and_calculate
     response = FreightService.calculation_cubage(params[:uf],params[:generator_id])
     if response.class == Hash
